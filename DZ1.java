0941c6fb77bd42950af2d3c478d3c5bec0c47455
«Olga» <osa_007@mail.ru>
2022-12-14 13:37:10 +0300

    added HW1

---
+++ DZ1.java
@@ -0,0 +1,11 @@
+package DZ1.Java;
+
+import java.util.Random;
+
+public class DZ1 {
+    public static void main(String[] args) {
+        int i = new Random().nextInt(2001);
+        System.out.println(i);
+    }
+    
+}
